b0f51feaba1d1b59c47a29b16dd8c0a1578c9e98
vedantath <vedant.athale@gmail.com>
2025-06-17 20:13:54 -0500

    alt tag

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -27,7 +27,7 @@ const page = () => {
             <p>Captrix simplifies screen recording. Whether you’re doing a brief walkthrough or a detailed presentation, 
               it’s quick, seamless, and lets you share your videos instantly.</p>
             <article>
-              <Image src="/assets/images/vedant.jpg" alt="jason" width={64} height={64} className='rounded-full'/>
+              <Image src="/assets/images/vedant.JPG" alt="vedant" width={64} height={64} className='rounded-full'/>
               <div>
                 <h2>Vedant Athale</h2>
                 {/* <p>Title, Captrix</p> */}
